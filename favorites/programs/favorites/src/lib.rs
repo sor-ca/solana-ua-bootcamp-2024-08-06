7f3938ee40c769fade6169604e6d6cf660cb9ff5
sor-ca <mariagrandova@gmail.com>
2024-08-30 00:19:21 +0300

    task 3.1

--- favorites/programs/favorites/src/lib.rs
+++ favorites/programs/favorites/src/lib.rs
@@ -1,16 +1,73 @@
 use anchor_lang::prelude::*;
 
-declare_id!("3GyHVeksm844U5o6mTevca329BDet9HZee3jtmnfQc8G");
+declare_id!("2yQwG6tLmpoiuoHsJypPQ5eKHnHy8gwRkNiCWAohg6cS");
 
+// Anchor programs always use
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+// What we will put inside the Favorites PDA
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+}
+
+// When people call the set_favorites instruction, they will need to provide the accounts that will
+// be modified. This keeps Solana fast!
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump,
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
+
+// Our Solana program!
 #[program]
 pub mod favorites {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    // Our instruction handler! It sets the user's favorite number and color
+    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String) -> Result<()> {
+        let user_public_key = context.accounts.user.key();
+        msg!("Greetings from {}", context.program_id);
+        msg!(
+            "User {}'s favorite number is {} and favorite color is: {}",
+            user_public_key,
+            number,
+            color
+        );
+
+        context
+            .accounts
+            .favorites
+            .set_inner(Favorites { number, color });
         Ok(())
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+    // We can also add a get_favorites instruction to get the user's favorite number and color
+}
+// #[program]
+// pub mod favorites {
+//     use super::*;
+
+//     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+//         msg!("Greetings from: {:?}", ctx.program_id);
+//         Ok(())
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct Initialize {}
